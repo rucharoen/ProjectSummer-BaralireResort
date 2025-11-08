65283c9b80ac15589ab0708b1b27634066c1c2c8
Rucharoen <rucharoen.nbu@northbkk.ac.th>
2025-11-08 23:46:04 +0700

    keep only project-forntend

--- app/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-// app/routes/auth.routes.js
-const controller = require("../controllers/auth.controller");
-
-module.exports = (app) => {
-  app.post("/api/auth/signin", controller.signin);
-  app.post("/api/auth/logout", controller.logout);   // ถ้าต้องการ
-  app.get("/api/auth/me", controller.me);            // ถ้าต้องการ
-  app.post("/api/hash", controller.hashPassword);    // ยูทิลิตี้
-};
